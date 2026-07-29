c5fe9fa54abdc01321ef54fef0557c51ffca1172
Jan Bońkowski <devbonkowski@gmail.com>
2026-07-29 22:44:39 +0200

    Improved navigation: utilising side nav, routing-based features, current context presentation

---
+++ MealPlanner.UI/Models/CalendarWeek.cs
@@ -0,0 +1,10 @@
+﻿namespace MealPlanner.UI.Models;
+
+public sealed class CalendarWeek
+{
+    public int WeekNumber { get; set; }
+
+    public List<DateTime> Days { get; set; } = [];
+        
+    public bool Current => Days.Contains(DateTime.Today);
+}
