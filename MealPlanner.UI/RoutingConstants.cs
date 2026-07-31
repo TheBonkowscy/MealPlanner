dfc4522986bcd08057b792469a43dfe0744347ea
Jan Bońkowski <devbonkowski@gmail.com>
2026-07-31 22:26:56 +0200

    Read Edit menu from backend

---
+++ MealPlanner.UI/RoutingConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace MealPlanner.UI;
+
+public static class RoutingConstants
+{
+    public static class Menus
+    {
+        private const string BasePath = "menus";
+        public const string Details = BasePath + "/day/";
+        public const string Edit = BasePath + "/edit/";
+        public const string Create = BasePath + "/create/";
+
+        public static string DetailsForDate(DateOnly date) => $"/{Details}{date:yyyy-MM-dd}";
+
+        public static string CreateForDate(DateOnly date) => $"/{Create}{date:yyyy-MM-dd}";
+
+        public static string EditForDate(DateOnly date) => $"/{Edit}{date:yyyy-MM-dd}";
+    }
+}
