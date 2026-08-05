3755b82af34918b33da5b065eb24d8d94f270feb
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-05 20:13:54 +0200

    MEAL-21: Fix empty menus being improperly qualified as non-existing (#15)
    * Ignore meals when deciding whether menu exists
    
    * Require passing list of meals when creating menu for a day. Added shared project for tests
    
    * [WIP] Added menu state service to handle cross-page state management
    
    * Refresh the navbar after creating the menu to reflect changes
    
    * Fixed bug with menu improperly reporting when 6 months ago happens in the middle of the week

---
+++ MealPlanner.UI/Services/MenuStateService.cs
@@ -0,0 +1,17 @@
+﻿namespace MealPlanner.UI.Services;
+
+public interface IMenuStateService
+{
+    event Action OnMenuChanged;
+    void NotifyMenuChanged();
+}
+
+public class MenuStateService : IMenuStateService
+{
+    public event Action? OnMenuChanged;
+    
+    public void NotifyMenuChanged()
+    {
+        OnMenuChanged?.Invoke();
+    }
+}
