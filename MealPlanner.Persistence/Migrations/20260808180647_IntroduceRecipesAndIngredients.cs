7a8e66b3d40f658199f2ee3132689e04f30be592
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-08 20:22:39 +0200

    Updated database migration to introduce Recipes

---
+++ MealPlanner.Persistence/Migrations/20260808180647_IntroduceRecipesAndIngredients.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MealPlanner.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntroduceRecipesAndIngredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Meals",
+                table: "Meals");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Meals");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Meals",
+                newName: "Order");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Order",
+                table: "Meals",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MenuId",
+                table: "Meals",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "Meals",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Meals",
+                table: "Meals",
+                columns: new[] { "MenuId", "RecipeId" });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientUnit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientUnit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientUnit_Ingredient_Id",
+                        column: x => x.Id,
+                        principalTable: "Ingredient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeStep",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Order = table.Column<int>(type: "integer", nullable: false),
+                    Instructions = table.Column<string>(type: "text", nullable: false),
+                    RecipeId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeStep", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecipeStep_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsedIngredient",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "integer", nullable: false),
+                    IngredientId = table.Column<int>(type: "integer", nullable: false),
+                    UnitId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsedIngredient", x => new { x.RecipeId, x.IngredientId, x.UnitId });
+                    table.ForeignKey(
+                        name: "FK_UsedIngredient_IngredientUnit_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "IngredientUnit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsedIngredient_Ingredient_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsedIngredient_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_RecipeId",
+                table: "Meals",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeStep_RecipeId",
+                table: "RecipeStep",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsedIngredient_IngredientId",
+                table: "UsedIngredient",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsedIngredient_UnitId",
+                table: "UsedIngredient",
+                column: "UnitId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_Menus_MenuId",
+                table: "Meals",
+                column: "MenuId",
+                principalTable: "Menus",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_Recipes_RecipeId",
+                table: "Meals",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_Menus_MenuId",
+                table: "Meals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_Recipes_RecipeId",
+                table: "Meals");
+
+            migrationBuilder.DropTable(
+                name: "RecipeStep");
+
+            migrationBuilder.DropTable(
+                name: "UsedIngredient");
+
+            migrationBuilder.DropTable(
+                name: "IngredientUnit");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Meals",
+                table: "Meals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Meals_RecipeId",
+                table: "Meals");
+
+            migrationBuilder.DropColumn(
+                name: "MenuId",
+                table: "Meals");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Meals");
+
+            migrationBuilder.RenameColumn(
+                name: "Order",
+                table: "Meals",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Meals",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Meals",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Meals",
+                table: "Meals",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                columns: table => new
+                {
+                    MenuId = table.Column<int>(type: "integer", nullable: false),
+                    MealId = table.Column<int>(type: "integer", nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => new { x.MenuId, x.MealId });
+                    table.ForeignKey(
+                        name: "FK_MenuItems_Meals_MealId",
+                        column: x => x.MealId,
+                        principalTable: "Meals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenuItems_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_MealId",
+                table: "MenuItems",
+                column: "MealId");
+        }
+    }
+}
