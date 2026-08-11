ad0654b31163479ef594984feb2a67a89dd801a6
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-11 19:29:15 +0200

    Added new DbSet for Ingredients

--- MealPlanner.Persistence/MealPlannerDbContext.cs
+++ MealPlanner.Persistence/MealPlannerDbContext.cs
@@ -10,6 +10,7 @@ public class MealPlannerDbContext : DbContext
     public virtual DbSet<Menu> Menus { get; set; }
     public virtual DbSet<Recipe> Recipes { get; set; }
     public virtual DbSet<Meal> Meals { get; set; }
+    public virtual DbSet<Ingredient> Ingredients { get; set; }
 
     public MealPlannerDbContext(DbContextOptions<MealPlannerDbContext> options) : base(options)
     {
