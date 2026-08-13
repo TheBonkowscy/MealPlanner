036a1513183d6cf1ab410ac322301f863ed21231
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-13 19:21:15 +0200

    Extend client to allow creating recipes

---
+++ MealPlanner.Client/ICreateRecipes.cs
@@ -0,0 +1,9 @@
+﻿using MealPlanner.Shared.Recipes.Requests;
+using MealPlanner.Shared.Recipes.Responses;
+
+namespace MealPlanner.Client;
+
+public interface ICreateRecipes
+{
+    Task<CreateRecipeResponse> CreateRecipe(CreateRecipeRequest createRecipeRequest, CancellationToken cancellationToken);
+}
