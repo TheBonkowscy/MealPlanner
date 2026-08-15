638294d8ea4f7493d743bc1b4a5e8c1bc2a428cf
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-15 16:12:22 +0200

    Added unit and integration tests

--- Tests/MealPlanner.API.Tests/RecipesIntegrationTests.cs
+++ Tests/MealPlanner.API.Tests/RecipesIntegrationTests.cs
@@ -2,10 +2,13 @@
 using AwesomeAssertions;
 using MealPlanner.API.Tests.Shared;
 using MealPlanner.Domain;
+using MealPlanner.Domain.Ingredients;
 using MealPlanner.Shared.Menus;
 using MealPlanner.Shared.Recipes;
+using MealPlanner.Shared.Recipes.Requests;
 using MealPlanner.Shared.Recipes.Responses;
 using MealPlanner.Tests.Shared.Factories;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace MealPlanner.API.Tests;
@@ -17,7 +20,7 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
     public async Task Get_ReturnsAllMeals_WhenNoQueryProvided()
     {
         // Arrange
-        await AddMealToDatabase(TestRecipes.Create("Burger"));
+        await AddRecipeToDatabase(TestRecipes.Create("Burger"));
 
         // Act
         var result = await Client.GetAsync(Constants.RecipesRoute);
@@ -34,7 +37,7 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
     {
         // Arrange
         var uniqueMealName = "Unique Sandwich";
-        await AddMealToDatabase(TestRecipes.Create(uniqueMealName));
+        await AddRecipeToDatabase(TestRecipes.Create(uniqueMealName));
 
         // Act
         var result = await Client.GetAsync($"{Constants.RecipesRoute}?q=sandw");
@@ -51,7 +54,7 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
     public async Task Get_ReturnsEmptyList_WhenNoMatchesFound()
     {
         // Arrange
-        await AddMealToDatabase(TestRecipes.Create("Pizza"));
+        await AddRecipeToDatabase(TestRecipes.Create("Pizza"));
 
         // Act
         var result = await Client.GetAsync($"{Constants.RecipesRoute}?q=salad");
@@ -61,6 +64,46 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
         var response = await result.Content.ReadFromJsonAsync<GetRecipesResponse>();
         response.Should().NotBeNull();
         response!.Recipes.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Post_ReturnsOk_WhenRecipeIsCreated()
+    {
+        // Arrange
+        var testIngredient = TestIngredients.Create();
+        DatabaseContext.Ingredients.Add(testIngredient);
+        await DatabaseContext.SaveChangesAsync();
+        var request = CreateNewRecipeRequest($"New Recipe_{Guid.NewGuid()}", testIngredient);
+
+        // Act
+        var result = await Client.PostAsJsonAsync(Constants.RecipesRoute, request);
+        
+        // Assert
+        result.EnsureSuccessStatusCode();
+        var response = await result.Content.ReadFromJsonAsync<CreateRecipeResponse>();
+        response.Should().NotBeNull();
+    }
+
+    private static CreateRecipeRequest CreateNewRecipeRequest(string recipeName, Ingredient ingredient) =>
+        new(recipeName,
+            1,
+            [new AddIngredientRequest(ingredient.Id, 1, ingredient.ApplicableUnits.First().ToString())],
+            [new AddStepRequest(1, "Test")]);
+
+    [Fact]
+    public async Task Post_ReturnsBadRequest_WhenRecipeIsNotValid()
+    {
+        // Arrange
+        var testIngredient = TestIngredients.Create();
+        var request = CreateNewRecipeRequest($"New Recipe_{Guid.NewGuid()}", testIngredient);
+        request.Ingredients.Clear();
+        request.Steps.Clear();
+
+        // Act
+        var result = await Client.PostAsJsonAsync(Constants.RecipesRoute, request);
+        
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 
     [Fact]
@@ -78,7 +121,7 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
     {
         // Arrange 
         var recipe = TestRecipes.Create("Pizza");
-        await AddMealToDatabase(recipe);
+        await AddRecipeToDatabase(recipe);
         
         // Act
         var result = await Client.GetAsync($"{Constants.RecipesRoute}/{recipe.Id}");
@@ -87,7 +130,21 @@ public class RecipesIntegrationTests(MealPlannerWebApplicationFactory factory) :
         result.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 
-    private async Task AddMealToDatabase(Recipe recipe)
+    [Fact]
+    public async Task Delete_ReturnsNoContent()
+    {
+        // Arrange
+        var recipe = TestRecipes.Create();
+        await AddRecipeToDatabase(recipe);
+        
+        // Act
+        var result = await Client.DeleteAsync($"{Constants.RecipesRoute}/{recipe.Id}");
+        
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    private async Task AddRecipeToDatabase(Recipe recipe)
     {
         await DatabaseContext.Recipes.AddAsync(recipe);
         await DatabaseContext.SaveChangesAsync();
---
+++ Tests/MealPlanner.Services.Tests/RecipeDeleterTests.cs
@@ -0,0 +1,44 @@
+using MealPlanner.Domain;
+using MealPlanner.Persistence;
+using MealPlanner.Services.Recipes;
+using MealPlanner.Tests.Shared.Factories;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace MealPlanner.Services.Tests;
+
+public class RecipeDeleterTests
+{
+    private static readonly Recipe PreExistingRecipe = TestRecipes.Create("Test Recipe");
+    private readonly Mock<MealPlannerDbContext> _ctx;
+    private readonly RecipeDeleter _sut;
+    
+    public RecipeDeleterTests()
+    {
+        _ctx = new Mock<MealPlannerDbContext>();
+        _ctx.Setup(x => x.Recipes).ReturnsDbSet([PreExistingRecipe]);
+        _ctx.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
+        
+        _sut = new RecipeDeleter(_ctx.Object);
+    }
+    
+    [Fact]
+    public async Task Deletes_WhenMenuDoesNotExist()
+    {
+        // Act
+        await _sut.Delete(Random.Shared.Next(99, 1000), CancellationToken.None);
+        
+        // Assert
+        _ctx.Verify(x => x.Recipes.Remove(It.IsAny<Recipe>()), Times.Never);
+    }
+    
+    [Fact]
+    public async Task Deletes_WhenMenuExists()
+    {
+        // Act
+        await _sut.Delete(PreExistingRecipe.Id, CancellationToken.None);
+        
+        // Assert
+        _ctx.Verify(x => x.Recipes.Remove(It.IsAny<Recipe>()), Times.Once);
+    }
+}
