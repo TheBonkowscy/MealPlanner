20e8c0ab0e2ef96ec0d9c2fdfc9362cc18cb9c39
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-16 21:38:37 +0200

    Updated UI to handle `Servings` parameter

--- MealPlanner.UI/Models/MealDto.cs
+++ MealPlanner.UI/Models/MealDto.cs
@@ -7,4 +7,5 @@ public class MealDto : IReorderable
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Order { get; set; }
+    public int Servings { get; set; }
 }
