25d372f6c3efe1fa5c95f1cdc45a53a60fd9f910
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-18 22:47:04 +0200

    Added new service to update the recipe step

--- MealPlanner.Services/Recipes/Steps/RecipeStepUpdater.cs
+++ MealPlanner.Services/Recipes/Steps/RecipeStepUpdater.cs
@@ -1,5 +1,7 @@
-﻿using MealPlanner.Shared.Recipes.Requests;
+﻿using MealPlanner.Persistence;
+using MealPlanner.Shared.Recipes.Requests;
 using MealPlanner.Shared.Recipes.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace MealPlanner.Services.Recipes.Steps;
 
@@ -8,10 +10,21 @@ public interface IUpdateRecipeStep
     Task<GetRecipeDetailsResponse> UpdateStep(int recipeId, int stepId, UpdateRecipeStepRequest request, CancellationToken cancellationToken);
 }
 
-public class RecipeStepUpdater : IUpdateRecipeStep
+public class RecipeStepUpdater(MealPlannerDbContext ctx, RecipeMapper recipeMapper) : IUpdateRecipeStep
 {
-    public Task<GetRecipeDetailsResponse> UpdateStep(int recipeId, int stepId, UpdateRecipeStepRequest request, CancellationToken cancellationToken)
+    public async Task<GetRecipeDetailsResponse> UpdateStep(int recipeId, int stepId, UpdateRecipeStepRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var recipe = await ctx.Recipes
+            .Include(recipe => recipe.Steps)
+            .FirstOrDefaultAsync(x => x.Id == recipeId, cancellationToken);
+        if (recipe is null)
+        {
+            throw new InvalidOperationException("Recipe could not be found");   // TODO: custom exceptions?
+        }
+
+        recipe.UpdateStep(stepId, request.Order, request.Instructions);
+        await ctx.SaveChangesAsync(cancellationToken);
+
+        return recipeMapper.ToDetails(recipe);
     }
 }
---
+++ Tests/MealPlanner.Services.Tests/Recipes/RecipeStepUpdaterTests.cs
@@ -0,0 +1,85 @@
+﻿using AwesomeAssertions;
+using MealPlanner.Domain;
+using MealPlanner.Persistence;
+using MealPlanner.Services.Recipes;
+using MealPlanner.Services.Recipes.Steps;
+using MealPlanner.Shared.Recipes.Requests;
+using MealPlanner.Tests.Shared.Factories;
+using Microsoft.Extensions.Localization;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace MealPlanner.Services.Tests.Recipes;
+
+public class RecipeStepUpdaterTests
+{
+    private readonly RecipeStepUpdater _sut;
+
+    private readonly List<Recipe> _recipes = [];
+    private readonly List<RecipeStep> _steps = [];
+
+    public RecipeStepUpdaterTests()
+    {
+        var localizer = new Mock<IStringLocalizer<Translations>>();
+        
+        var ctx = new Mock<MealPlannerDbContext>();
+        ctx.Setup(x => x.Recipes).ReturnsDbSet(_recipes);
+        ctx.Setup(x => x.RecipeSteps).ReturnsDbSet(_steps);
+        var measureUnitMapper = new MeasureUnitMapper(localizer.Object);
+        _sut = new RecipeStepUpdater(ctx.Object, new RecipeMapper(measureUnitMapper));
+    }
+
+    [Fact]
+    public async Task UpdateStep_Throws_WhenRecipeWasNotFound()
+    {
+        // Arrange
+        var recipe = TestRecipes.Create();
+        var step = recipe.Steps[0];
+        var request = new UpdateRecipeStepRequest(step.Id,  step.Order, step.Instructions); 
+        
+        // Act
+        var updateStep = () => _sut.UpdateStep(recipe.Id, step.Id, request, CancellationToken.None);
+        
+        // Assert
+        await updateStep.Invoking(x => x.Invoke())
+            .Should()
+            .ThrowAsync<InvalidOperationException>()
+            .WithMessage("Recipe could not be found");
+    }
+
+    [Fact]
+    public async Task UpdateStep_Throws_WhenStepWasNotFound()
+    {
+        // Arrange
+        var recipe = TestRecipes.Create();
+        _recipes.Add(recipe);
+        var step = recipe.Steps[0];
+        var request = new UpdateRecipeStepRequest(999,  step.Order, step.Instructions); 
+        
+        // Act
+        var updateStep = () => _sut.UpdateStep(recipe.Id, request.Id, request, CancellationToken.None);
+        
+        // Assert
+        await updateStep.Invoking(x => x.Invoke())
+            .Should()
+            .ThrowAsync<InvalidOperationException>()
+            .WithMessage("Recipe step could not be found");
+    }
+
+    [Fact]
+    public async Task UpdateStep_Succeeds()
+    {
+        // Arrange
+        var recipe = TestRecipes.Create();
+        _recipes.Add(recipe);
+        var step = recipe.Steps[0];
+        var request = new UpdateRecipeStepRequest(step.Id,  step.Order, step.Instructions + "a");
+        
+        // Act
+        var result = await _sut.UpdateStep(recipe.Id, request.Id, request, CancellationToken.None);
+        
+        // Assert
+        var stepResponse = result.Steps.First(x => x.Id == step.Id);
+        stepResponse.Instructions.Should().Be(request.Instructions);
+    }
+}
