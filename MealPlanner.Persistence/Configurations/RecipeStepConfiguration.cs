ff7ab385a3c55125f0ae3cdc421437da92c06769
Jan Bońkowski <devbonkowski@gmail.com>
2026-08-24 22:40:06 +0200

    MEAL-52: Updated persistence configuration for `RecipeStep` (#36)

---
+++ MealPlanner.Persistence/Configurations/RecipeStepConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using MealPlanner.Domain.Recipes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealPlanner.Persistence.Configurations;
+
+public class RecipeStepConfiguration : IEntityTypeConfiguration<RecipeStep>
+{
+    public void Configure(EntityTypeBuilder<RecipeStep> builder)
+    {
+        builder.ToTable("RecipeSteps");
+        
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Order).IsRequired();
+        builder.Property(x => x.Instructions).IsRequired();
+    }
+}
